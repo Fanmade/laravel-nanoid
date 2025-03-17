471ec81a60980572a44d6eb7220b356ee04dd197
erossdev <78756041+erossdev@users.noreply.github.com>
2025-03-17 10:46:19 -0400

    fix: pup 8.4 deprecation

--- src/helpers.php
+++ src/helpers.php
@@ -3,7 +3,7 @@
 use Fanmade\NanoId\Facades\NanoID;
 
 if (!function_exists('nano_id')) {
-    function nano_id(?int $length = null, string $alphabet = null): string
+    function nano_id(?int $length = null, ?string $alphabet = null): string
     {
         return NanoID::generate($length, $alphabet);
     }
